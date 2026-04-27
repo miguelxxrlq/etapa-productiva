442eafa9c2d606d990f30fdfb3d61f4d3376cd63
unknown <miguelangelgomezromero12443@gmail.com>
2026-04-27 16:17:07 -0500

    Componentes vacios del proyecto creados

---
+++ src/componentes/PiePagina.jsx
@@ -0,0 +1,9 @@
+// PiePagina.jsx — información inferior de la app 
+function PiePagina() {
+    return (
+        <footer>
+            <p>Creado por [Elcomepiedrasvolador] · SENA 2026</p>
+        </footer>
+    )
+}
+export default PiePagina 
